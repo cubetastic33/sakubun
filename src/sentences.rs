be92596edcea3340a0418ff5ea3cdb9e78259229
aravk <aravkasi@gmail.com>
2021-02-05 18:56:21 +0530

    Fix code that allows only known kanji

--- src/sentences.rs
+++ src/sentences.rs
@@ -1,12 +1,13 @@
 use rand::prelude::*;
+use std::collections::HashSet;
 use std::error::Error;
 use std::fs;
 
 pub fn get_sentences() -> Result<Vec<[String;2]>, Box<dyn Error>> {
-    // TODO allow custom number of kanji required for a sentence to be included
     let mut sentences = Vec::new();
     let mut rng = thread_rng();
 
+    let known_kanji: HashSet<_> = fs::read_to_string("known_kanji.txt")?.chars().collect();
     // Read the sentences and shuffle the order
     let records  = fs::read_to_string("sentences.csv")?;
     let mut records: Vec<_> = records.split('\n').collect();
@@ -14,21 +15,17 @@ pub fn get_sentences() -> Result<Vec<[String;2]>, Box<dyn Error>> {
 
     for result in records {
         let record: Vec<_> = result.split('\t').collect();
+        if record.len() != 4 {
+            continue;
+        }
         let jap_sentence = record[1];
         let eng_sentence = record[2];
-        let kanji_in_sentence = record[3];
+        let kanji_in_sentence: HashSet<_> = record[3].chars().collect();
 
-        let known_kanji = fs::read_to_string("known_kanji.txt")?;
-
-        // If it has any of the known kanji, add it to the list
-        for kanji in known_kanji.chars() {
-            if kanji_in_sentence.contains(kanji) {
-                // Add the sentence to the list since it has kanji they know
-                sentences.push([jap_sentence.to_string(), eng_sentence.to_string()]);
-                break;
-            }
+        // TODO allow custom number of kanji required for a sentence to be included
+        if kanji_in_sentence.is_subset(&known_kanji) && kanji_in_sentence.len() >= 1 {
+            sentences.push([jap_sentence.to_string(), eng_sentence.to_string()]);
         }
-
         // Once we've collected 30 sentences, we can exit the loop
         if sentences.len() == 30 {
             break;
