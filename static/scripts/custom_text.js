2f1a0e07d8f7ab1f8cf8263df0461f8009cca369
aravk <aravkasi@gmail.com>
2025-06-15 18:55:11 -0400

    Make custom text work with synced kanji

--- static/scripts/custom_text.js
+++ static/scripts/custom_text.js
@@ -2,6 +2,14 @@
 var kuroshiro = new Kuroshiro();
 kuroshiro.init(new KuromojiAnalyzer({dictPath: '/dict'}));
 
+async function get_known_kanji() {
+  const { data: {user} = {} } = await client.auth.getUser();
+  if (user) {
+    let { data } = await client.from('known_kanji').select('known_kanji').eq('user_id', user.id);
+    return new Set(data.length ? data[0].known_kanji : []);
+  } else return new Set(localStorage.getItem('known_kanji'));
+}
+
 function is_superset(set, subset) {
   for (let elem of subset) {
     if (!set.has(elem)) {
@@ -11,18 +19,16 @@ function is_superset(set, subset) {
   return true;
 }
 
-$('form').submit(e => {
+$('form').submit(async e => {
   e.preventDefault();
-  kuroshiro.convert($('textarea').val(), {mode: 'furigana', to: 'hiragana'}).then(result => {
-    let known_kanji = new Set(localStorage.getItem('known_kanji'));
-    $('#result').html(result.replaceAll('\n', '<br>'));
+  const known_kanji = await get_known_kanji();
+  const result = await kuroshiro.convert($('textarea').val(), {mode: 'furigana', to: 'hiragana'});
+  $('#result').html(result.replaceAll('\n', '<br>'));
 
-    $('ruby').each(function () {
-      let word = new Set($(this).html().split('<rp>')[0]);
-      console.log(word, known_kanji);
-      if (is_superset(known_kanji, word)) {
-        $(this).children().remove();
-      }
-    });
+  $('ruby').each(function () {
+    let word = new Set($(this).html().split('<rp>')[0]);
+    if (is_superset(known_kanji, word)) {
+      $(this).children().remove();
+    }
   });
 });
